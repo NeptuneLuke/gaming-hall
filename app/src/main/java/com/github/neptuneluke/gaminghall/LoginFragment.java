6be20419a55df99f1a5bdfc60b6c2583564eeac5
Luca Cortinovis <luca1cortinovis@gmail.com>
2024-06-21 01:08:49 +0200

    Added fragment login UI

---
+++ app/src/main/java/com/github/neptuneluke/gaminghall/LoginFragment.java
@@ -0,0 +1,31 @@
+package com.github.neptuneluke.gaminghall;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LoginFragment extends Fragment {
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    public static LoginFragment newInstance() {
+        return new LoginFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
