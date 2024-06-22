8bcc40857ffb23ad61ed7e9d0ac91a0854fb70cd
Luca Cortinovis <luca1cortinovis@gmail.com>
2024-06-22 14:42:18 +0200

    Added Constants.java and Util.java help classes

---
+++ app/src/main/java/com/github/neptuneluke/gaminghall/Constants.java
@@ -0,0 +1,14 @@
+package com.github.neptuneluke.gaminghall;
+
+public class Constants {
+
+    // Strings
+    public static final int USERNAME_MIN_LENGTH = 1;
+    public static final int USERNAME_MAX_LENGTH = 30;
+    public static final int PASSWORD_MIN_LENGTH = 6;
+    public static final int PASSWORD_MAX_LENGTH = 30;
+
+    // Firebase Realtime Database
+    public static final String FIREBASE_REALTIME_DATABASE = "https://gaminghall-e3416-default-rtdb.firebaseio.com";
+
+}
---
+++ app/src/main/java/com/github/neptuneluke/gaminghall/Util.java
@@ -0,0 +1,60 @@
+package com.github.neptuneluke.gaminghall;
+
+import android.util.Log;
+
+import org.apache.commons.validator.routines.EmailValidator;
+
+public class Util {
+
+    // Login/Registration
+    public static boolean isValidUsername(String username) {
+        if(username.isEmpty()
+                || username.contains(" ")
+                || username.length() < Constants.USERNAME_MIN_LENGTH
+                || username.length() > Constants.USERNAME_MAX_LENGTH) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isValidEmail(String email) {
+        if(!EmailValidator.getInstance().isValid((email))) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isValidPassword(String password) {
+        if(password.isEmpty()
+                || password.contains(" ")
+                || password.length() < Constants.PASSWORD_MIN_LENGTH
+                || password.length() > Constants.PASSWORD_MAX_LENGTH) {
+            return false;
+        }
+        return true;
+    }
+
+
+    // Logs
+    public static void LOG_MESSAGE_D(String TAG, String message) {
+
+        message.toUpperCase().trim();
+        String separator = "";
+        for(int i=0; i < message.length(); ++i) {
+            separator += "-";
+        }
+        Log.d(TAG,separator);
+        Log.d(TAG,message);
+    }
+
+    public static void LOG_MESSAGE_I(String TAG, String message) {
+
+        message.toUpperCase().trim();
+        String separator = "";
+        for(int i=0; i < message.length(); ++i) {
+            separator += "-";
+        }
+        Log.i(TAG,separator);
+        Log.i(TAG,message);
+    }
+}
