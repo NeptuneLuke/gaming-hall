d03edaaa9e6b56af9dba4bb5b5604f4dfad2a674
Luca Cortinovis <luca1cortinovis@gmail.com>
2024-06-26 09:35:17 +0200

    Updated recyclerview temporary solution

--- app/src/main/java/com/github/neptuneluke/gaminghall/RecyclerViewLatestGames_Adapter.java
+++ app/src/main/java/com/github/neptuneluke/gaminghall/RecyclerViewLatestGames_Adapter.java
@@ -1,9 +1,11 @@
 package com.github.neptuneluke.gaminghall;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,43 +14,54 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RecyclerViewLatestGames_Adapter extends RecyclerView.Adapter<RecyclerViewLatestGames_Adapter.StringViewHolder> {
+public class RecyclerViewLatestGames_Adapter extends RecyclerView.Adapter<RecyclerViewLatestGames_Adapter.TestViewHolder> {
 
     private final Context context;
-    private final List<String> items = new ArrayList<>();
+    private List<String> text_items = new ArrayList<>();
+    private List<Drawable> image_items = new ArrayList<>();
 
     public RecyclerViewLatestGames_Adapter(Context context) {
         this.context = context;
     }
 
-    public class StringViewHolder extends RecyclerView.ViewHolder {
+    public class TestViewHolder extends RecyclerView.ViewHolder {
         TextView textView;
+        ImageView imageView;
 
-        StringViewHolder(View itemView) {
+        TestViewHolder(View itemView) {
             super(itemView);
             textView = itemView.findViewById(R.id.textview_in_recyclerview_item); // Adjust ID as needed
+            imageView = itemView.findViewById(R.id.imageview_in_recyclerview_item);
+            // imageView.setImageResource(R.drawable.ic_app);
         }
     }
 
     @NonNull
     @Override
-    public StringViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public TestViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(context).inflate(R.layout.recyclerview_item, parent, false); // Adjust layout as needed
-        return new StringViewHolder(itemView);
+        return new TestViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull StringViewHolder holder, int position) {
-        holder.textView.setText(items.get(position));
+    public void onBindViewHolder(@NonNull TestViewHolder holder, int position) {
+
+        holder.textView.setText(text_items.get(position));
+        // holder.imageView.setImageResource(R.drawable.ic_app);
+        holder.imageView.setImageDrawable(image_items.get(position));
+
     }
 
     @Override
     public int getItemCount() {
-        return items.size();
+        return text_items.size();
     }
 
     public void addItem(String newItem) {
-        items.add(newItem);
-        notifyItemInserted(items.size() - 1);
+        text_items.add(newItem);
+        Drawable d = this.context.getResources().getDrawable(R.drawable.ic_app);
+        image_items.add(d);
+        notifyItemInserted(text_items.size() - 1);
     }
+
 }
